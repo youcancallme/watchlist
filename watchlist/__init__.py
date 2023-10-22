dd9494ba4f2cfa6d42e54e2ab57c9e42b74c346f
yxlzd <y6d_5z2l3x4@163.com>
2023-10-22 21:55:00 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -14,7 +14,8 @@ else:  # 否则使用四个斜线
 app = Flask(__name__)
 # 设置一个随机的安全密钥
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 
