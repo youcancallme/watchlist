213a42d2bc43c047cc1a00594c57c490739acd28
yxlzd <y6d_5z2l3x4@163.com>
2023-10-22 21:19:11 +0800

    Add unit test with unittest

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ def index():#主页
     movies = Movie.query.all()  # 读取所有电影记录
     return render_template('index.html',movies=movies)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return f'User: {escape(name)}'
 
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
@@ -112,17 +112,17 @@ def delete(movie_id):
     flash('Item deleted.')
     return redirect(url_for('index'))  # 重定向回主页
 
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+# @app.route('/test')
+# def test_url_for():
+#     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+#     print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+#     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+#     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+#     print(url_for('user_page', name='peter'))  # 输出：/user/peter
+#     print(url_for('test_url_for'))  # 输出：/test
+#     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+#     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+#     return 'Test page'
 
 
 class Movie(db.Model):  # 表名将会是 movie
